f3751f53500fda45157e0bdf7e9bc45e98d590ae
Caleb Jones <cjones@air-watch.com>
2014-09-30 21:41:18 -0400

    Narrowed Shell Issue
    this only occurs when the variables are the result of a command.
    Example:
    
    a='hello '
    b='world'
    c=$a$b
    echo $c
    
    This works. Output: hello world
    
    Provider=$(adb shell getprop gsm.operator.alpha)
    Model=$(adb shell getprop ro.product.model)
    c=$Provider$Model
    echo $c
    
    Fails. Output: Nexus 5e

--- Test_StringConcatenate.sh
+++ Test_StringConcatenate.sh
@@ -1,20 +1,14 @@
 #Fetches device details and logs them into variables.
 #TO DO change the awk to actually edit the variable.
-ProductBrand=$(adb shell getprop ro.product.brand)
+
 Provider=$(adb shell getprop gsm.operator.alpha)
-Device=$(adb shell getprop ro.product.device)
 Model=$(adb shell getprop ro.product.model)
-clear
-empty=' '
 c=$Provider$Model
 echo $c
-echo
-echo $ProductBrand
-echo $Model 
-echo $Provider
+
 
 echo
-a='hello'
+a='hello '
 b='world'
 c=$a$b
 echo $c
